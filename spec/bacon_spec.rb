a6578477b0cbd3cd398ccf0f8450a26ca376e423
Nell Shamrell <nshamrell@bluebox.net>
2012-01-16 19:21:48 -0800

    adding rest of files

---
+++ spec/bacon_spec.rb
@@ -0,0 +1,13 @@
+require 'bacon'
+
+describe Bacon do
+
+  subject { Bacon.new }
+
+  its(:edible?) { should be_true }
+
+  it "expired!" do
+    subject.expired!
+    subject.should_not be_edible
+  end
+end
